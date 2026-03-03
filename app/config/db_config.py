e2e9a1d4797a7bc11d6c78e1ce00d1cce50d7483
keinerSD <kokeiner2007@gmail.com>
2026-03-03 11:36:57 -0500

    Simplify database connection by removing sslmode
    Removed unnecessary sslmode parameter from database connection.

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -4,10 +4,8 @@ import os
 def get_db_connection():
     database_url = os.getenv("DATABASE_URL")
 
-    conn = psycopg2.connect(
-        database_url,
-        sslmode="require"  # importante para Railway
-    )
+    conn = psycopg2.connect( database_url)
 
     return conn
+
 
