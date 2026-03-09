7d1bf34132c3ebbdb230496d4dc6ee4f10f79dc9
keinerSD <kokeiner2007@gmail.com>
2026-03-09 15:59:05 -0500

    Update CORS origins in main.py
    Added a new origin for CORS middleware configuration.

--- app/main.py
+++ app/main.py
@@ -16,7 +16,8 @@ app = FastAPI()
 origins = [
     #"http://localhost.tiangolo.com",
     #"https://localhost.tiangolo.com",
-    "http://localhost"
+    "http://localhost",
+    "http://localhost:5173"
     #"http://localhost:8080",
 ]
 
@@ -38,5 +39,6 @@ app.include_router(vital_sign_router)
 app.include_router(query_router)
 app.include_router(appointment_router)
 app.include_router(nursing_staff_router)
+
 
 
