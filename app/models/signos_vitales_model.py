e97c60f71f7f5f2992dd9a544cc4d115292b5887
keinerSD <kokeiner2007@gmail.com>
2026-03-09 21:20:49 -0500

    Rename model and update vital signs fields

---
+++ app/models/signos_vitales_model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class SignosVitales(BaseModel):
+    id_signos: int = None
+    id_consulta: int
+
+    presion_arterial: str
+    temperatura: float
+    peso: float
+    altura: float
+    saturacion_oxigeno: float
+    frecuencia_cardiaca: int
+    tipo_sangre: str
