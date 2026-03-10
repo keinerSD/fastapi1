bda74bc99eb64b0dff261f0260160e1eeec0ec62
Causa-IA <acausilperez@gmail.com>
2026-03-09 23:49:44 -0500

    Make id_rol optional in Rol model

--- app/models/rol_model.py
+++ app/models/rol_model.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
-
+from typing import Optional
 class Rol(BaseModel):
-    id_rol: int = None
+    id_rol: Optional[int] = None
     nombre: str
     descripcion: str
     acceso_privilegiado: bool
+
