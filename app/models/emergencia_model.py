90c32289e1bfcfed65d8259767dfe713d18e61e2
keinerSD <kokeiner2007@gmail.com>
2026-03-09 23:48:48 -0500

    Make id_emergencia optional in Emergencia model

--- app/models/emergencia_model.py
+++ app/models/emergencia_model.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
 import datetime
+from typing import Optional
 
 class Emergencia(BaseModel):
-    id_emergencia: int = None
+    id_emergencia: Optional[int] = None
     id_usuario: int
     id_estudiante: int
 
     fecha: datetime.datetime
     descripcion: str
     atencion_prestada: str
+
