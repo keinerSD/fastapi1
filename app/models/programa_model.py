f4069327ef1579a10149fca33d8ed592127a5891
Causa-IA <acausilperez@gmail.com>
2026-03-09 23:50:22 -0500

    Make id_programa optional in Programa model

--- app/models/programa_model.py
+++ app/models/programa_model.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
-
+from typing import Optional
 class Programa(BaseModel):
-    id_programa: int = None
+    id_programa: Optional[int] = None
     id_facultad: int
     nombre: str
     descripcion: str
+
