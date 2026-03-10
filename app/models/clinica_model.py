b7b3fbc2af9917541b7a310d6c2290cd688de605
keinerSD <kokeiner2007@gmail.com>
2026-03-09 23:45:42 -0500

    Change id_clinica to Optional in Clinica model

--- app/models/clinica_model.py
+++ app/models/clinica_model.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Clinica(BaseModel):
-    id_clinica: int = None
+    id_clinica: Optional[int] = None
     nombre: str
     ubicacion: str
+
