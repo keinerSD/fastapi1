3479b74fb569bbd208e600309ec11279e1dea7b6
Causa-IA <acausilperez@gmail.com>
2026-03-09 23:49:00 -0500

    Make id_signos optional in SignosVitales model

--- app/models/signos_vitales_model.py
+++ app/models/signos_vitales_model.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
-
+from typing import Optional
 class SignosVitales(BaseModel):
-    id_signos: int = None
+    id_signos: Optional[int] = None
     id_consulta: int
 
     presion_arterial: str
@@ -11,4 +11,5 @@ class SignosVitales(BaseModel):
     saturacion_oxigeno: float
     frecuencia_cardiaca: float
     tipo_sangre: str
+
 
