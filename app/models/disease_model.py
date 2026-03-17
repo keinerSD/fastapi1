25988b8fe9a07afdf68a4cf88756788868b34233
Causa-IA <acausilperez@gmail.com>
2026-03-17 12:15:55 -0500

    Delete app/models/disease_model.py

--- app/models/disease_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class SymptomsInput(BaseModel):
-    symptoms: List[str]
-
-class DiseasePrediction(BaseModel):
-    disease: str
-    probability: float
