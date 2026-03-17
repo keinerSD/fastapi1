882c4e9a2dbe69d6151da2f69492e4a053234bb2
Causa-IA <acausilperez@gmail.com>
2026-03-17 13:38:08 -0500

    Modify CORS settings in main.py
    Updated CORS middleware to allow all origins.

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # o ["*"] para permitir todos
+    allow_origins=["*"],  # o ["*"] para permitir todos
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
